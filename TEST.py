af328e3c3ea7c4e7d7aa428d8a900fac87507ea9
НЯМ <gosepkov983@gmail.com>
2026-03-26 17:18:20 +0300

    Add files via upload

--- TEST.py
+++ TEST.py
@@ -1,57 +1,86 @@
 import sys
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QTableWidget, 
-                             QTableWidgetItem, QPushButton, QVBoxLayout, 
-                             QWidget, QHeaderView)
+from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
 
 app = QApplication(sys.argv)
 window = QMainWindow()
-window.setWindowTitle("Таблица с кнопками")
+window.setWindowTitle("Таблица")
 window.resize(800, 500)
 
-# Центральный виджет
-central = QWidget()
-window.setCentralWidget(central)
+table = QTableWidget(5, 4)
+window.setCentralWidget(table)
 
-# Layout
-layout = QVBoxLayout()
-central.setLayout(layout)
-
-# Таблица
-table = QTableWidget()
-layout.addWidget(table)
-
-# Настройка таблицы: 5 строк, 4 столбца
-rows = 5
-cols = 4
-
-table.setRowCount(rows)
-table.setColumnCount(cols)
-
-# Заголовки столбцов
 table.setHorizontalHeaderLabels(["Название", "1", "2", "3"])
 
-# Заполняем левый столбец (названия строк, можно редактировать)
-names = ["Строка 1", "Строка 2", "Строка 3", "Строка 4", "Строка 5"]
+# Строка 1
+table.setItem(0, 0, QTableWidgetItem("Строка 1"))
+table.item(0, 0).setFlags(table.item(0, 0).flags() | Qt.ItemFlag.ItemIsEditable)
 
-for i in range(rows):
-    item = QTableWidgetItem(names[i])
-    item.setFlags(item.flags() | Qt.ItemFlag.ItemIsEditable)
-    table.setItem(i, 0, item)
+btn1 = QPushButton("1")
+table.setCellWidget(0, 1, btn1)
 
-# Заполняем остальные столбцы кнопками с цифрами
-for row in range(rows):
-    for col in range(1, cols):
-        button = QPushButton(str(col))
-        button.setEnabled(False)  # кнопка неактивна, не нажимается
-        table.setCellWidget(row, col, button)
+btn2 = QPushButton("2")
+table.setCellWidget(0, 2, btn2)
 
-# Настройка размеров
-table.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeMode.Stretch)
-for i in range(1, cols):
-    table.setColumnWidth(i, 100)
+btn3 = QPushButton("3")
+table.setCellWidget(0, 3, btn3)
 
-table.verticalHeader().setDefaultSectionSize(60)
+# Строка 2
+table.setItem(1, 0, QTableWidgetItem("Строка 2"))
+table.item(1, 0).setFlags(table.item(1, 0).flags() | Qt.ItemFlag.ItemIsEditable)
+
+btn1 = QPushButton("1")
+table.setCellWidget(1, 1, btn1)
+
+btn2 = QPushButton("2")
+table.setCellWidget(1, 2, btn2)
+
+btn3 = QPushButton("3")
+table.setCellWidget(1, 3, btn3)
+
+# Строка 3
+table.setItem(2, 0, QTableWidgetItem("Строка 3"))
+table.item(2, 0).setFlags(table.item(2, 0).flags() | Qt.ItemFlag.ItemIsEditable)
+
+btn1 = QPushButton("1")
+table.setCellWidget(2, 1, btn1)
+
+btn2 = QPushButton("2")
+table.setCellWidget(2, 2, btn2)
+
+btn3 = QPushButton("3")
+table.setCellWidget(2, 3, btn3)
+
+# Строка 4
+table.setItem(3, 0, QTableWidgetItem("Строка 4"))
+table.item(3, 0).setFlags(table.item(3, 0).flags() | Qt.ItemFlag.ItemIsEditable)
+
+btn1 = QPushButton("1")
+table.setCellWidget(3, 1, btn1)
+
+btn2 = QPushButton("2")
+table.setCellWidget(3, 2, btn2)
+
+btn3 = QPushButton("3")
+table.setCellWidget(3, 3, btn3)
+
+# Строка 5
+table.setItem(4, 0, QTableWidgetItem("Строка 5"))
+table.item(4, 0).setFlags(table.item(4, 0).flags() | Qt.ItemFlag.ItemIsEditable)
+
+btn1 = QPushButton("1")
+table.setCellWidget(4, 1, btn1)
+
+btn2 = QPushButton("2")
+table.setCellWidget(4, 2, btn2)
+
+btn3 = QPushButton("3")
+table.setCellWidget(4, 3, btn3)
+
+table.setColumnWidth(0, 200)
+table.setColumnWidth(1, 100)
+table.setColumnWidth(2, 100)
+table.setColumnWidth(3, 100)
 
 window.show()
 sys.exit(app.exec())
