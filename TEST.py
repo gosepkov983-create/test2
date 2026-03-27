961846afae01be8a466a99bd572197310db249d1
НЯМ <gosepkov983@gmail.com>
2026-03-27 14:18:37 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -13,7 +13,7 @@ window.setCentralWidget(table)
 table.setHorizontalHeaderLabels(["Название", "1", "2", "3"])
 
 # Строка 1
-table.setItem(0, 0, QTableWidgetItem("Строка 1"))
+table.setItem(0, 0, QTableWidgetItem(" 1"))
 table.item(0, 0).setFlags(table.item(0, 0).flags() | Qt.ItemFlag.ItemIsEditable)
 
 btn1 = QPushButton("1")
@@ -26,7 +26,7 @@ btn3 = QPushButton("3")
 table.setCellWidget(0, 3, btn3)
 
 
-table.setItem(1, 0, QTableWidgetItem("Строка 2"))
+table.setItem(1, 0, QTableWidgetItem(" 2"))
 table.item(1, 0).setFlags(table.item(1, 0).flags() | Qt.ItemFlag.ItemIsEditable)
 
 btn1 = QPushButton("1")
@@ -38,7 +38,7 @@ table.setCellWidget(1, 2, btn2)
 btn3 = QPushButton("3")
 table.setCellWidget(1, 3, btn3)
 
-table.setItem(2, 0, QTableWidgetItem("Строка 3"))
+table.setItem(2, 0, QTableWidgetItem("3"))
 table.item(2, 0).setFlags(table.item(2, 0).flags() | Qt.ItemFlag.ItemIsEditable)
 
 btn1 = QPushButton("1")
@@ -51,7 +51,7 @@ btn3 = QPushButton("3")
 table.setCellWidget(2, 3, btn3)
 
 
-table.setItem(3, 0, QTableWidgetItem("Строка 4"))
+table.setItem(3, 0, QTableWidgetItem(" 4"))
 table.item(3, 0).setFlags(table.item(3, 0).flags() | Qt.ItemFlag.ItemIsEditable)
 
 btn1 = QPushButton("1")
@@ -64,7 +64,7 @@ btn3 = QPushButton("3")
 table.setCellWidget(3, 3, btn3)
 
 
-table.setItem(4, 0, QTableWidgetItem("Строка 5"))
+table.setItem(4, 0, QTableWidgetItem("5"))
 table.item(4, 0).setFlags(table.item(4, 0).flags() | Qt.ItemFlag.ItemIsEditable)
 
 btn1 = QPushButton("1")
