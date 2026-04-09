df44a1b9fbc140df32b60dee5c00c2545e1e35ab
НЯМ <gosepkov983@gmail.com>
2026-04-09 17:45:58 +0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,85 +1,88 @@
 import sys
+import os
+from pathlib import Path
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt
+
+
+def get_apps():
+    paths = [
+        Path(os.getenv("PROGRAMDATA")) / "Microsoft/Windows/Start Menu/Programs",
+        Path(os.getenv("APPDATA")) / "Microsoft/Windows/Start Menu/Programs"
+    ]
+
+    BLACKLIST = [
+        "uninstall", "install", "update", "helper",
+        "apphost", "runtime", "service", "crash",
+        "debug", "setup", "maintenance", "feedback"
+    ]
+
+    apps = []
+
+    for base in paths:
+        if base.exists():
+            for file in base.rglob("*.lnk"):
+                name = file.name.lower()
+
+                if any(word in name for word in BLACKLIST):
+                    continue
+
+                apps.append(file)
+
+    apps = list(set(apps))
+    apps.sort(key=lambda x: x.name.lower())
+    return apps
+
+
+
+def open_app(path):
+    try:
+        os.startfile(path)
+    except Exception as e:
+        print("Er:", e)
+
 
 app = QApplication(sys.argv)
 window = QMainWindow()
 window.setWindowTitle("Приложение")
-window.resize(800, 500)
-
-table = QTableWidget(5, 4)
-window.setCentralWidget(table)
-
-table.setHorizontalHeaderLabels(["Название", "1", "2", "3"])
-
-# Строка 1
-table.setItem(0, 0, QTableWidgetItem(" 1"))
-table.item(0, 0).setFlags(table.item(0, 0).flags() | Qt.ItemFlag.ItemIsEditable)
-
-btn1 = QPushButton("1")
-table.setCellWidget(0, 1, btn1)
-
-btn2 = QPushButton("2")
-table.setCellWidget(0, 2, btn2)
-
-btn3 = QPushButton("3")
-table.setCellWidget(0, 3, btn3)
+window.resize(1000, 500)
 
 
-table.setItem(1, 0, QTableWidgetItem(" 2"))
-table.item(1, 0).setFlags(table.item(1, 0).flags() | Qt.ItemFlag.ItemIsEditable)
-
-btn1 = QPushButton("1")
-table.setCellWidget(1, 1, btn1)
-
-btn2 = QPushButton("2")
-table.setCellWidget(1, 2, btn2)
-
-btn3 = QPushButton("3")
-table.setCellWidget(1, 3, btn3)
-
-table.setItem(2, 0, QTableWidgetItem("3"))
-table.item(2, 0).setFlags(table.item(2, 0).flags() | Qt.ItemFlag.ItemIsEditable)
-
-btn1 = QPushButton("1")
-table.setCellWidget(2, 1, btn1)
-
-btn2 = QPushButton("2")
-table.setCellWidget(2, 2, btn2)
-
-btn3 = QPushButton("3")
-table.setCellWidget(2, 3, btn3)
+main_widget = QWidget()
+layout = QHBoxLayout(main_widget)
+window.setCentralWidget(main_widget)
 
 
-table.setItem(3, 0, QTableWidgetItem(" 4"))
-table.item(3, 0).setFlags(table.item(3, 0).flags() | Qt.ItemFlag.ItemIsEditable)
-
-btn1 = QPushButton("1")
-table.setCellWidget(3, 1, btn1)
-
-btn2 = QPushButton("2")
-table.setCellWidget(3, 2, btn2)
-
-btn3 = QPushButton("3")
-table.setCellWidget(3, 3, btn3)
 
 
-table.setItem(4, 0, QTableWidgetItem("5"))
-table.item(4, 0).setFlags(table.item(4, 0).flags() | Qt.ItemFlag.ItemIsEditable)
+app_table = QTableWidget()
+app_table.setColumnCount(1)
+app_table.setHorizontalHeaderLabels(["Приложения"])
 
-btn1 = QPushButton("1")
-table.setCellWidget(4, 1, btn1)
 
-btn2 = QPushButton("2")
-table.setCellWidget(4, 2, btn2)
+layout.addWidget(app_table)
 
-btn3 = QPushButton("3")
-table.setCellWidget(4, 3, btn3)
+apps = get_apps()
 
-table.setColumnWidth(0, 200)
-table.setColumnWidth(1, 100)
-table.setColumnWidth(2, 100)
-table.setColumnWidth(3, 100)
+def show_apps(app_list):
+    app_table.setRowCount(len(app_list))
+    for row, app_path in enumerate(app_list):
+        item = QTableWidgetItem(app_path.stem)
+        item.setData(1000, str(app_path))
+        app_table.setItem(row, 0, item)
+
+    app_table.setColumnWidth(0, 700)
+
+
+show_apps(apps)
+
+
+def launch_app(row, column):
+    item = app_table.item(row, 0)
+    path = item.data(1000)
+    open_app(path)
+
+app_table.cellClicked.connect(launch_app)
 
 window.show()
 sys.exit(app.exec())
